617b4b260b3db3218e847207b6ba5311a4146a7b
KaMeGoD <jcgarciadev@gmail.com>
2023-06-21 19:00:11 -0400

    algunas animaciones

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,36 @@ import { Pasatiempos } from './components/Pasatiempos';
 import { Proyectos } from './components/Proyectos';
 import { Tecnologias } from './components/Tecnologias';
 import { Contacto } from './components/Contacto';
+import ScrollReveal from 'scrollreveal';
 
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 function App() {
   const bienvenidoRef = useRef<HTMLDivElement>(null);
   const pasatiemposRef = useRef<HTMLDivElement>(null);
-  const scrollToBotton = () =>{
+
+  const scrollToBottonBienvenido = () =>{
     pasatiemposRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }
 
+  useEffect(()=>{
+    const sr = ScrollReveal();
+    sr.reveal(bienvenidoRef.current! , {
+      origin: 'top',
+      duration: 1000,
+      distance: '50px'
+    });
+    sr.reveal(pasatiemposRef.current!, {
+      origin: 'left',
+      duration: 1000,
+      distance: '30px',
+    })
+  },[]);
+
+  
   return (
     <>
-      <Bienvenido onClick={scrollToBotton} refBienvenidos={bienvenidoRef}/>
+      <Bienvenido onClick={scrollToBottonBienvenido} refBienvenidos={bienvenidoRef}/>
       <Pasatiempos refPasatiempos={pasatiemposRef}/>
       <Proyectos />
       <Tecnologias />
