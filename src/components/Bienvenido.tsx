03117fcc556b1c51134d5c6537ec7f7789517d23
KaMeGoD <jcgarciadev@gmail.com>
2023-06-27 16:31:08 -0400

    arreglado responsive bienvenido y tecnologias

--- src/components/Bienvenido.tsx
+++ src/components/Bienvenido.tsx
@@ -6,31 +6,32 @@ import bloques from '../assets/img/bloques.png'
 
 // 
 
-interface PropsBienvenido{
-  refBienvenidos : React.LegacyRef<HTMLDivElement>;
-  onClick : (e:React.MouseEvent<HTMLButtonElement>) => void
+interface PropsBienvenido {
+  refBienvenidos: React.LegacyRef<HTMLDivElement>;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-export const Bienvenido: React.FC<PropsBienvenido>  = ({refBienvenidos, onClick}) => {
+export const Bienvenido: React.FC<PropsBienvenido> = ({ refBienvenidos, onClick }) => {
 
-  return(
+  return (
     <>
-    <div className='bienvenido' ref={refBienvenidos}>
-      <div className='bienvenido-top'>
-        <img className='logo' src={logo} alt="logo" />
-        <h2 className='profesion'>Desarrollador Web</h2>
-      </div>
-      <div className='bienvenido-main'>
-        <div className='izquierda'>
-          <h1 className='nombre'>Juan <br />García</h1>
-          <button onClick={onClick} className='btn-start hvr-pop'>Empecemos</button>
+      <div className='bienvenido' ref={refBienvenidos}>
+        <div className='bienvenido-top'>
+          <img className='logo' src={logo} alt="logo" />
+          <h2 className='profesion'>Desarrollador Web</h2>
         </div>
-        <div className='derecha'>
-          <img className='saludo' src={saludo} alt="saludos" />
-          <img className='bloque' src={bloques} alt="bloques" />
+        <div className='bienvenido-main'>
+          <div className='izquierda'>
+            <h1 className='nombre'>Juan <br />García</h1>
+            <button onClick={onClick} className='btn-start hvr-pop'>Empecemos</button>
+          </div>
+          <div className='derecha'>
+            <div className='bloque'>
+              <img className='saludo' src={saludo} alt="saludos" />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
