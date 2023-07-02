1ea49fe148ef7801119ff975a50806752271f16f
KaMeGoD <jcgarciadev@gmail.com>
2023-07-02 10:44:05 -0400

    agregando proyectos paisesapi

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 export const Proyectos = () =>{
   const [efecto1, setEfecto1] = useState(false);
   const [efecto2, setEfecto2] = useState(false);
+  const [efecto3, setEfecto3] = useState(false);
 
   const handleMouseOver1 = () =>{
     setEfecto1(true);
@@ -19,6 +20,12 @@ export const Proyectos = () =>{
   const handleMouseOut2 = () =>{
     setEfecto2(false);
   }
+  const handleMouseOver3 = () =>{
+    setEfecto2(true);
+  }
+  const handleMouseOut3 = () =>{
+    setEfecto2(false);
+  }
 
   return(
     <>
@@ -30,7 +37,7 @@ export const Proyectos = () =>{
         <div className="derecha">
           <Proyecto claseEfecto={efecto1 ? 'proyecto-wrap-true': 'proyecto-wrap-false'} MouseOut={handleMouseOut1} MouseOver={handleMouseOver1} titulo='Calculadora de Propinas' git='https://github.com/KaMeGoD/Calculadora-de-Propina.git' netlify='https://calculadora-propina-jcdev.netlify.app/' clase='calculadora'/>
           <Proyecto claseEfecto={efecto2 ? 'proyecto-wrap-true': 'proyecto-wrap-false'} MouseOut={handleMouseOut2} MouseOver={handleMouseOver2} titulo='Piedra Papel y Tijeras' git='https://github.com/KaMeGoD/Piedra_Papel_o_Tijeras.git' netlify='https://piedrapapeltijeras-jcdev.netlify.app' clase='juegoPPT'/>
-          {/* <Proyecto claseEfecto={efecto ? 'proyecto-wrap-true': 'proyecto-wrap-false'} MouseOut={handleMouseOut} MouseOver={handleMouseOver} titulo='Calculadora de Propinas' git='#' netlify='#' clase='ala'/> */}
+          <Proyecto claseEfecto={efecto3 ? 'proyecto-wrap-true': 'proyecto-wrap-false'} MouseOut={handleMouseOut3} MouseOver={handleMouseOver3} titulo='Calculadora de Propinas' git='https://github.com/KaMeGoD/Api-Paises.git' netlify='https://apipaices-jcdev.netlify.app/' clase='ala'/>
         </div>
       </div>
 
